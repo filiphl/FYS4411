7a41267cd95591c31f92dff2611c14e424db1e5e
filiphl <filiphenriklarsen@gmail.com>
2016-01-30 09:42:35 +0100

    Close to finish a wavefunction. Problem b

--- Project_1/solver.cpp
+++ Project_1/solver.cpp
@@ -7,7 +7,18 @@ Solver::Solver()
     }
 
     Solver::wavefunction(mat r){
-
+        double psi = 1;
+        for (int p=0; p<m_nParticles; p++){
+            double r_single = 0;
+            double g = 0;
+            for (int d=0; d<m_nDimensions; d++){
+                r_single += r(p,d) * r(p,d);
+            }
+            g = exp(-m_alpha * r_single );
+            double psi = psi*g;
+        total += r_single;
+        }
+        return exp(-m_alpha * total )
     }
 }
 
