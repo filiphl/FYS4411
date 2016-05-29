9dd6272a6c3df3e5efb83f36fbbdc6c361f4178e
filiphl <filiphenriklarsen@gmail.com>
2016-05-29 12:24:21 +0200

    Fixed function

--- Project_2/pythonScripts/std.py
+++ Project_2/pythonScripts/std.py
@@ -1,11 +1,10 @@
 from math import sqrt
 from sys import argv
 
-def std(tau, dt, var):
-    return sqrt(1+2*tau/dt)*var
+def std(tau, var):
+    return sqrt((1+2*tau)*var)
 
 if __name__ == '__main__':
     tau = float(argv[1])
-    dt  = float(argv[2])
-    var = float(argv[3])
-    print std(tau, dt, var)
+    var = float(argv[2])
+    print std(tau, var)
